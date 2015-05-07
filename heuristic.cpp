d66a8d7bd1866fabb11c47c462b42321f5851441
qwangmsk <josephw10000@gmail.com>
2015-05-06 23:29:34 -0400

    Reformat part of the code to make it look clean

--- heuristic.cpp
+++ heuristic.cpp
@@ -10,36 +10,32 @@
 
 /*-------- Global Variables. Begin ------------*/
 
-char	 	ABC_str_20[] 	= 	{'A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y'};
-char	 	ABC_str_4[]  	= 	{'A','C','G','T'};
-char	 	ABC_idx_20[] 	= 	{ 0,0,1,2,3,4,5,6,7,7,8,9,10,11,11,12,13,14,15,16,16,17,18,18,19,19};
-char	 	ABC_idx_4[] 	= 	{ 0,0,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3};
+char	ABC_str_20[] = {'A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y'};
+char	ABC_str_4[]  = {'A','C','G','T'};
+char	ABC_idx_20[] = { 0,0,1,2,3,4,5,6,7,7,8,9,10,11,11,12,13,14,15,16,16,17,18,18,19,19};
+char	ABC_idx_4[]  = { 0,0,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3};
 
-int			MAXSTRSIZE;
-int			MAXSTR;
-int			ABC_len;
-char*		ABC_str;
-char*		ABC_idx;
+int	MAXSTRSIZE;
+int	MAXSTR;
+int	ABC_len;
+char*	ABC_str;
+char*	ABC_idx;
 
+char**	data;
+int*	str_lngth;
+int	cur_n_str = 0;
+char	data_file_fullname[128];
 
-char**		data;
-int*		str_lngth;
-int			cur_n_str = 0;
-
-char		data_file_fullname[128];
-
-std::vector <short**>		cur_T;
-std::vector <short**>		cur_M;
+std::vector <short**>	cur_T;
+std::vector <short**>	cur_M;
 
 /*-------- AVL Tree.  Begin ------------*/
 
 int NUMDIM  = 1;
 int NUMBYTE = 1;
 
-typedef struct _Node
-{
-	union 
-	{
+typedef struct _Node {
+	union {
 		unsigned char cXYZ[16]; 
 		unsigned short sXYZ[8];
 
@@ -48,14 +44,13 @@ typedef struct _Node
 		#else
 			unsigned long lXYZ[2];
 		#endif
-
 	} uXYZ;
 
 	short			h;
-	int				fg;
+	int			fg;
 	bool			expended;
 	_Node			*parent;
-	TREE_ENTRY(_Node)	 linkage;
+	TREE_ENTRY(_Node)	linkage;
 } Node;
 
 typedef TREE_HEAD(_Tree, _Node) Tree;
@@ -63,34 +58,25 @@ typedef TREE_HEAD(_Tree, _Node) Tree;
 TREE_DEFINE(_Node, linkage);
 
 
-_Node *Node_new(_Node *rhs)
-{
+_Node *Node_new(_Node *rhs) {
 	_Node *self = (_Node *)malloc(sizeof(_Node));
-	if (rhs)
+	if (rhs) {
 		memcpy(self, rhs, sizeof(_Node));
-
+	}
 	return self;
 }
 
-void Node_free(_Node *rhs)
-{
+void Node_free(_Node *rhs) {
 	if (rhs) free(rhs);
 }
 
-int Node_compare(_Node *lhs, _Node *rhs)
-{
-	if (NUMDIM == 1)
-	{
+int Node_compare(_Node *lhs, _Node *rhs) {
+	if (NUMDIM == 1) {
 		return lhs->uXYZ.lXYZ[0] > rhs->uXYZ.lXYZ[0] ? 1 : (lhs->uXYZ.lXYZ[0] < rhs->uXYZ.lXYZ[0] ? -1 : 0);
-	}
-	else
-	{
-		if (lhs->uXYZ.lXYZ[0] == rhs->uXYZ.lXYZ[0])
-		{
+	} else {
+		if (lhs->uXYZ.lXYZ[0] == rhs->uXYZ.lXYZ[0]) {
 			return lhs->uXYZ.lXYZ[1] > rhs->uXYZ.lXYZ[1] ? 1 : (lhs->uXYZ.lXYZ[1] < rhs->uXYZ.lXYZ[1] ? -1 : 0);
-		}
-		else
-		{
+		} else {
 			return lhs->uXYZ.lXYZ[0] > rhs->uXYZ.lXYZ[0] ? 1 : -1;
 		}
 	}
@@ -109,11 +95,9 @@ int Node_compare(_Node *lhs, _Node *rhs)
 std::vector <_Node*> myHeap(1000, 0);
 int heap_sz = 0;
 
-void SiftDown()
-{
+void SiftDown() {
 	int parent = 0;
-	while(parent<heap_sz)
-	{
+	while(parent<heap_sz) {
 		int l = 2 * parent + 1;
 		int r = l + 1;
 		int flag = parent;
@@ -124,9 +108,9 @@ void SiftDown()
 		if (r<heap_sz && (myHeap[r]->fg > myHeap[flag]->fg)) {
 			flag = r;
 		}
-		if (flag == parent)
+		if (flag == parent) {
 			break;
-		else{
+		} else {
 			Node* temp = myHeap[flag];
 			myHeap[flag] = myHeap[parent];
 			myHeap[parent] = temp;
@@ -135,16 +119,14 @@ void SiftDown()
 	}
 }
 
-void SiftUp()
-{
+void SiftUp() {
 	int son = heap_sz-1;
-	while(son > 0)
-	{
+	while(son > 0) {
 		int parent = son / 2;
 	 
-		if (myHeap[parent]->fg > myHeap[son]->fg)
+		if (myHeap[parent]->fg > myHeap[son]->fg) {
 			break;
-		else{
+		} else {
 			Node* temp = myHeap[son];
 			myHeap[son] = myHeap[parent];
 			myHeap[parent] = temp;
@@ -153,8 +135,7 @@ void SiftUp()
 	}
 }
 
-void HeapInsert(_Node* p)
-{
+void HeapInsert(_Node* p) {
 	if (heap_sz == myHeap.size())
 		myHeap.push_back(p);
 	else
@@ -164,10 +145,8 @@ void HeapInsert(_Node* p)
 	SiftUp();
 }
 
-_Node* HeapRemove()
-{
+_Node* HeapRemove() {
 	_Node* ret = myHeap[0];
-
 	myHeap[0] = myHeap[heap_sz-1];
 	heap_sz--;
 
@@ -177,11 +156,9 @@ _Node* HeapRemove()
 
 /*-------- functions. Begin ------------*/
 
-void ComputeM()
-{
+void ComputeM() {
 	int i;
-	for (i=0; i<(MAXSTR+1)/2; i++)
-	{
+	for (i=0; i<(MAXSTR+1)/2; i++) {
 		int i1 = i << 1;
 		int i2 = 1+ (i << 1);
 		if (i2>=MAXSTR) i2=0;
@@ -196,10 +173,8 @@ void ComputeM()
 		for (x=0; x<=str_lngth[i2]; x++)
 			Mi[str_lngth[i1]][x] = 0;
 
-		for (x=str_lngth[i1]-1; x>=0; x--)
-		{
-			for (y=str_lngth[i2]-1; y>=0; y--)
-			{
+		for (x=str_lngth[i1]-1; x>=0; x--) {
+			for (y=str_lngth[i2]-1; y>=0; y--) {
 				if (data[i1][x] == data[i2][y])
 					Mi[x][y] = Mi[x+1][y+1]+1;
 				else
@@ -209,11 +184,9 @@ void ComputeM()
 	}
 }
 
-void ComputeF(_Node* p)
-{
+void ComputeF(_Node* p) {
 	int h = cur_M[0][0][0];
-	for (int i=0; i<(MAXSTR+1)/2; i++)
-	{
+	for (int i=0; i<(MAXSTR+1)/2; i++) {
 		int i1 = i << 1;
 		int i2 = 1+ (i << 1);
 		if (i2>=MAXSTR) i2=0;
@@ -228,11 +201,10 @@ void ComputeF(_Node* p)
 	p->fg = ((h + p->h) << 16) | h;
 }
 
-int CommonSeq(_Node* p)
-{
-	if (!p)
+int CommonSeq(_Node* p) {
+	if (!p) {
 		return 0;
-	
+	}
 	int mlcs_len = CommonSeq(p->parent);
 		
 	if (NUMBYTE == 1)
@@ -245,8 +217,7 @@ int CommonSeq(_Node* p)
 
 /*-------- Main loop. Begin ------------*/
 
-void Mainloop()
-{	
+void Mainloop() {	
 	long closedset_sz = 0;
 	long openset_sz   = 0;
 	
@@ -270,40 +241,32 @@ void Mainloop()
 	HeapInsert(p);
 	openset_sz++;
 
-	while(true)
-	{
+	while(true) {
 		_Node* p = HeapRemove();
 		p->expended = true;
 		closedset_sz++;
 		openset_sz--;
 
-		if (p->h == 0)
-		{
+		if (p->h == 0) 	{
 			printf("\nThe MLCS is:\n");
 			int l = CommonSeq(p);
 			printf("\n\n|MLCS|=%d\n", l);
 			break;
 		}
 
-		for(int i=0; i<ABC_len; i++)
-		{ 
+		for(int i=0; i<ABC_len; i++) { 
 			 int j=0;
 
-			 if (NUMBYTE == 1)
-			 {
-				 while (j<MAXSTR)
-				 {
+			 if (NUMBYTE == 1) {
+				 while (j<MAXSTR) {
 					 temp->uXYZ.cXYZ[j] = cur_T[j][p->uXYZ.cXYZ[j]+1][i];
 
 					 if  (temp->uXYZ.cXYZ[j]==str_lngth[j]+1)
 						 break;
 					 j++; 
 				 }
-			 }
-			 else
-			 {
-				 while (j<MAXSTR)
-				 {
+			 } else {
+				 while (j<MAXSTR) {
 					 temp->uXYZ.sXYZ[j] = cur_T[j][p->uXYZ.sXYZ[j]+1][i];
 
 					 if  (temp->uXYZ.sXYZ[j]==str_lngth[j]+1)
@@ -317,18 +280,13 @@ void Mainloop()
 
 			_Node *q = TREE_FIND(&tree, _Node, linkage, temp);
 
-			if (q)	//node is already in tree
-			{
-				if (!q->expended && (q->fg & 0xffff) < (p->fg & 0xffff)+1)
-				{
+			if (q) { //node is already in tree
+				if (!q->expended && (q->fg & 0xffff) < (p->fg & 0xffff)+1) {
 					q->parent = p;
 					q->fg = ((q->h+(p->fg & 0xffff)+1) << 16) | ((p->fg & 0xffff)+1);
 				}
-			}
-			else
-			{
+			} else {
 				q = Node_new(temp);
-
 				q->parent = p;
 				q->fg = p->fg+1;
 				ComputeF(q);
@@ -351,8 +309,7 @@ void Mainloop()
 
 /*-------- Memory Management. Begin ------------*/
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	//***System.out.println("MLCS Version 1.0\n");
 	//get input parameters and initialize global variables
 	if(argc<3){
@@ -381,18 +338,14 @@ int main(int argc, char* argv[])
 
 	//check the content of the input sequence file
 	FILE *my_file = fopen(data_file_fullname, "r");
-	if (my_file == NULL)
-	{
+	if (my_file == NULL) {
 		printf("Empty string file: %s!\n", data_file_fullname);
 		return 1;
-	}
-	else
-	{
+	} else {
 		MAXSTRSIZE = 0;
 		char cur_line[10000];
 
-		while (!feof(my_file))  
-		{
+		while (!feof(my_file)) {
 			fscanf(my_file, "%s\n", cur_line);
 			//sscanf(cur_line, "%s", cur_line);
 			int len = strlen(cur_line);
@@ -414,14 +367,13 @@ int main(int argc, char* argv[])
 
 	//allocate memory for global variables
 	data = Allocate2DArray<char>(MAXSTR, MAXSTRSIZE+1);
-	str_lngth	 = new int[MAXSTR];
+	str_lngth = new int[MAXSTR];
 
 	//read input strings and do preprocessing
 	cur_n_str = 0;
 	int i, j;
 	my_file = fopen(data_file_fullname, "r");
-	while (!feof(my_file) && (cur_n_str < MAXSTR))  
-	{
+	while (!feof(my_file) && (cur_n_str < MAXSTR)) {
 		fscanf(my_file, "%s\n", data[cur_n_str]);
 		int len = strlen(data[cur_n_str]);
 		if (!len)continue;
@@ -431,15 +383,12 @@ int main(int argc, char* argv[])
 				
 		cur_T.push_back(Allocate2DArray<short>(len+1, ABC_len));
 		
-		for (i=0; i<ABC_len; i++)
-		{
+		for (i=0; i<ABC_len; i++) {
 			cur_T[cur_n_str][len][i] = len+1;
 		}
 
-		for (j=len-1; j>=0; j--)
-		{
-			for (i=0; i<ABC_len; i++)
-			{
+		for (j=len-1; j>=0; j--) {
+			for (i=0; i<ABC_len; i++) {
 				cur_T[cur_n_str][j][i] = cur_T[cur_n_str][j+1][i];
 			}
 			cur_T[cur_n_str][j][ABC_idx[data[cur_n_str][j]-'A']] = j;
@@ -464,8 +413,7 @@ int main(int argc, char* argv[])
 		Free2DArray<short>(temp);
 	}
 
-	for (i=0; i<(MAXSTR+1)/2; i++)
-	{
+	for (i=0; i<(MAXSTR+1)/2; i++) {
 		short** temp = cur_M[i];
 		Free2DArray<short>(temp);
 	}
